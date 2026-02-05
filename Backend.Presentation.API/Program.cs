ada74cfb984f8c8e447e30783bdf3a6a5701d64e
miguel <miguel.garay@hotmail.se>
2026-02-05 09:08:04 +0100

    Implemented cors configuration, get all heroes endpoint and heroes list

--- Backend.Presentation.API/Program.cs
+++ Backend.Presentation.API/Program.cs
@@ -1,7 +1,10 @@
+using Backend.Presentation.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -9,6 +12,26 @@ var app = builder.Build();
 app.MapOpenApi();
 
 app.UseHttpsRedirection();
+app.UseCors(c => c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
+app.MapGet("/api/heroes", () =>
+{
+
+
+
+    var heroes = new List<HeroDto>
+    {
+        new(
+        1, 
+        "Deam big. Achieve more. Visual Studio 2026", 
+        "Unleash our full potential with the world's most polular IDE for the professional developer"
+        )
+    };
+
+    return Results.Ok(heroes);
+});
+
 
 
 app.Run();
