f0fedc4a4225a1e26dbeb0c7188a2ec929f6e05a
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-25 15:55:40 +0100

    feature(teacher service interface):
    - ITeacherService created and finished

---
+++ CourseOnline.Application/Contracts/Teachers/ITeacherService.cs
@@ -0,0 +1,13 @@
+﻿using CourseOnline.Application.Teachers.DTOs.Inputs;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Contracts.Teachers;
+public interface ITeacherService
+{
+    Task<Teacher?> CreateTeacherAsync(CreateTeacherInput input, CancellationToken ct);
+    Task<Teacher?> GetTeacherByIdAsync(Guid id, CancellationToken ct);
+    Task<Teacher?> GetTeacherByEmail(string email, CancellationToken ct);
+    Task<IReadOnlyCollection<Teacher>> GetTeachersAsync(CancellationToken ct);
+    Task<Teacher?> UpdateTeacher(UpdateTeacherInput input, CancellationToken ct);
+    Task<bool> DeleteTeacherAsync(Guid id, CancellationToken ct);
+}
