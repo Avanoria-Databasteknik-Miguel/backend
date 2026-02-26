a589286ebf6d08f4c6fd8c62ac38c20d13924414
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-26 23:28:00 +0100

    feat(program factory):
    - Implemented

---
+++ CourseOnline.Application/Factories/ProgramFactory.cs
@@ -0,0 +1,9 @@
+﻿using CourseOnline.Application.Programs.DTOs.Inputs;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Factories;
+public static class ProgramFactory
+{
+    public static Program Create(CreateProgramInput input) => new(Guid.NewGuid(), input.Name, input.DurationWeeks, input.MaxStudents);
+
+}
