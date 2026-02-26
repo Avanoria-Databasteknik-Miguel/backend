651b59a173852c2bfadbd638d77cb5ddc62a992d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-26 16:24:57 +0100

    feat(Result):
    - Finished Result classes.

--- CourseOnline.Application/Common/Results/Result.cs
+++ CourseOnline.Application/Common/Results/Result.cs
@@ -1,4 +1,19 @@
 ﻿namespace CourseOnline.Application.Common.Results;
-public sealed record Result
+public sealed record Result(bool Success, ErrorTypes? ErrorType = null, string? ErrorMessage = null)
 {
+    public static Result Ok() => new(true);
+    public static Result BadRequest(string message) => new(false, ErrorTypes.BadRequest, message);
+    public static Result NotFound(string message) => new(false, ErrorTypes.NotFound, message);
+    public static Result Conflict(string message) => new(false, ErrorTypes.Conflict, message);
+    public static Result Unexpected(string message) => new(false, ErrorTypes.Unexpected, message);
+
 }
+
+public sealed record Result<T>(bool Success, T? Value = default, ErrorTypes? ErrorType = null, string? ErrorMessage = null)
+{
+    public static Result<T> Ok() => new(true);
+    public static Result<T> BadRequest(string message) => new(false, default, ErrorTypes.BadRequest, message);
+    public static Result<T> NotFound(string message) => new(false, default, ErrorTypes.NotFound, message);
+    public static Result<T> Conflict(string message) => new(false, default, ErrorTypes.Conflict, message);
+    public static Result<T> Unexpected(string message) => new(false, default, ErrorTypes.Unexpected, message);
+} 
