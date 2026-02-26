64faf78ee671f50e2b1f01e822db7b9158cb5ca1
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-26 23:02:46 +0100

    feat(created):
    - ProgramRepository class.

---
+++ CourseOnline.Infrastructure/Persistence/EFC/Repositories/Programs/ProgramRepository.cs
@@ -0,0 +1,8 @@
+﻿using CourseOnline.Domain.Models;
+using CourseOnline.Infrastructure.Persistence.Contexts;
+using CourseOnline.Infrastructure.Persistence.EFC.Entities;
+
+namespace CourseOnline.Infrastructure.Persistence.EFC.Repositories.Programs;
+public class ProgramRepository(CourseOnlineDbContext context) : RepositoryBase<Program, Guid, ProgramEntity, CourseOnlineDbContext>
+{
+}
