ff6775cd391ab883956cd6b51f2a5f1f97182449
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-26 22:52:17 +0100

    feat(Dtos):
    - Created and finished Dtos fpr Delete and update inputs.

---
+++ CourseOnline.Application/Programs/DTOs/Inputs/DeleteProgramInput.cs
@@ -0,0 +1,3 @@
+﻿namespace CourseOnline.Application.Programs.DTOs.Inputs;
+
+public sealed record DeleteProgramInput(Guid Id)
---
+++ CourseOnline.Application/Programs/DTOs/Inputs/UpdateProgramInput.cs
@@ -0,0 +1,2 @@
+﻿namespace CourseOnline.Application.Programs.DTOs.Inputs;
+public sealed record UpdateProgramInput(Guid Id, string Name, int? DurationWeeks, int? MaxStudents)
