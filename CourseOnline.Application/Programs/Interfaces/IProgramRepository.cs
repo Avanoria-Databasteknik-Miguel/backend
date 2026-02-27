c4e51da26e2cfe57799cb0c256f9fc188edc0cfd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-27 15:57:38 +0100

    refactor(modified return):
    - Returning nullable Program model.

--- CourseOnline.Application/Programs/Interfaces/IProgramRepository.cs
+++ CourseOnline.Application/Programs/Interfaces/IProgramRepository.cs
@@ -1,9 +1,8 @@
 ﻿using CourseOnline.Application.Common.Interfaces;
-using CourseOnline.Application.Common.Results;
 using CourseOnline.Domain.Models;
 
 namespace CourseOnline.Application.Programs.Interfaces;
 public interface IProgramRepository : IRepositoryBase<Program, Guid>
 {
-    Task<Result<Program>> GetByNameAsync(string name, CancellationToken ct);
+    Task<Program?> GetByNameAsync(string name, CancellationToken ct);
 }
