faf9fefac92d34f0bff236eb04a52eb935612f3c
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-27 17:56:03 +0100

    refactor(auditable-entity):
    - Implemented AuditableEntity inheritance in these classes

--- CourseOnline.Infrastructure/Persistence/EFC/Entities/TeacherEntity.cs
+++ CourseOnline.Infrastructure/Persistence/EFC/Entities/TeacherEntity.cs
@@ -1,12 +1,12 @@
-﻿namespace CourseOnline.Infrastructure.Persistence.EFC.Entities;
-public class TeacherEntity
+﻿using CourseOnline.Infrastructure.Common.Entities;
+
+namespace CourseOnline.Infrastructure.Persistence.EFC.Entities;
+public class TeacherEntity : AuditableEntity
 {
     public Guid Id { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string? ImageUrl { get; set; } = null!;
-    public byte[] RowVersion { get; set; } = null!;
-    public DateTime CreatedAtUtc { get; set; }
-    public DateTime ModifiedAtUtc { get; set; }
+
 }
