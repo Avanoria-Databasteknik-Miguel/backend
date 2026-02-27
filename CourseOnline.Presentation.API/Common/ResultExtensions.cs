6a905c68994cab2689cacdcc4e0c611aa6c16d61
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-27 18:51:22 +0100

    feature(HttpResult):
    - Implemented blueprint for HttpResults

---
+++ CourseOnline.Presentation.API/Common/ResultExtensions.cs
@@ -0,0 +1,33 @@
+﻿using CourseOnline.Application.Common.Results;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace CourseOnline.Presentation.API.Common;
+
+public static class ResultExtensions
+{
+    public static IResult ToHttpResult(this Result result)
+    {
+        if (result.Success) return Results.Ok();
+
+        return result.ErrorType switch
+        {
+            ErrorTypes.BadRequest => Results.BadRequest(result.ErrorMessage),
+            ErrorTypes.NotFound => Results.NotFound(result.ErrorMessage),
+            ErrorTypes.Conflict => Results.Conflict(result.ErrorMessage),
+            _ => Results.Problem(result.ErrorMessage)
+        };
+    }
+
+    public static IResult ToHttpResult<T>(this Result<T> result)
+    {
+        if (result.Success) return Results.Ok(result.Value);
+
+        return result.ErrorType switch 
+        {
+            ErrorTypes.BadRequest => Results.BadRequest(result.ErrorMessage),
+            ErrorTypes.NotFound => Results.NotFound(result.ErrorMessage),
+            ErrorTypes.Conflict => Results.Conflict(result.ErrorMessage),
+            _ => Results.Problem(result.ErrorMessage)
+        };
+    }
+}
