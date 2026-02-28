08041a0ac4670e099b6dfbc4c4eaa0ab62d54e95
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 09:03:29 +0100

    feat(student repo): Implemented with its logic repo-base

---
+++ CourseOnline.Infrastructure/Persistence/EFC/Repositories/Students/StudentRepository.cs
@@ -0,0 +1,30 @@
+﻿using CourseOnline.Application.Common.Results;
+using CourseOnline.Application.Students.Interfaces;
+using CourseOnline.Domain.Models;
+using CourseOnline.Infrastructure.Persistence.Contexts;
+using CourseOnline.Infrastructure.Persistence.EFC.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseOnline.Infrastructure.Persistence.EFC.Repositories.Students;
+
+public class StudentRepository(CourseOnlineDbContext context) : RepositoryBase<Student, Guid, StudentEntity, CourseOnlineDbContext>(context), IStudentRepository
+{
+    protected override StudentEntity ToEntity(Student model)
+    {
+        return new StudentEntity()
+        {
+            Id = model.Id,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Email = model.Email,
+            ImageUrl = model.ImageUrl,
+
+        };
+    }
+    protected override Student ToModel(StudentEntity entity)
+    {
+        return new Student(entity.Id, entity.FirstName, entity.LastName, entity.Email, entity.ImageUrl, entity.ProgramId);
+    }
+
+
+}
