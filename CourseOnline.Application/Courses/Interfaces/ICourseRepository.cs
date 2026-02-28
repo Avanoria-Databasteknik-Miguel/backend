2e828639712b66d48ae1a2bff5b46f094903b98d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 19:03:40 +0100

    feat(course): add ICourseRepository with GetByNameAsync

---
+++ CourseOnline.Application/Courses/Interfaces/ICourseRepository.cs
@@ -0,0 +1,8 @@
+﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Courses.Interfaces;
+public interface ICourseRepository : IRepositoryBase<Course, Guid>
+{
+    Task<Course?> GetByNameAsync(string name, CancellationToken ct);
+}
