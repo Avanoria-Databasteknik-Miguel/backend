c3d6519c0f7244356acbbfc18f1ee40ee6b483c2
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 08:53:56 +0100

    refactor(no byName method): Deleting GetByNameAsync for now.

--- CourseOnline.Application/Students/Interfaces/IStudentRepository.cs
+++ CourseOnline.Application/Students/Interfaces/IStudentRepository.cs
@@ -5,5 +5,5 @@ using CourseOnline.Domain.Models;
 namespace CourseOnline.Application.Students.Interfaces;
 public interface IStudentRepository : IRepositoryBase<Student, Guid>
 {
-    Task<Result<Program>> GetByNameAsync(string email, CancellationToken ct);
+
 }
