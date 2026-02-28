f21336daefdfee2c03919eca171f97fc5627dbc9
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 19:04:01 +0100

    feat(course): add CourseFactory for creating Course domain model

---
+++ CourseOnline.Application/Factories/CourseFactory.cs
@@ -0,0 +1,9 @@
+﻿using CourseOnline.Application.Courses.DTOs.Inputs;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Factories;
+
+public static class CourseFactory
+{
+    public static Course Create(CreateCourseInput input) => new(Guid.NewGuid(), input.Name, input.DurationWeeks, input.MaxStudents, input.TeacherId, input.ProgramId);
+}
