eba5b4bbfa14368be30d3790b908eeedad5776dd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 14:05:30 +0100

    refactor(email method):
    - Introduce GetbyEmail in student repository

--- CourseOnline.Infrastructure/Persistence/EFC/Repositories/Students/StudentRepository.cs
+++ CourseOnline.Infrastructure/Persistence/EFC/Repositories/Students/StudentRepository.cs
@@ -9,6 +9,13 @@ namespace CourseOnline.Infrastructure.Persistence.EFC.Repositories.Students;
 
 public class StudentRepository(CourseOnlineDbContext context) : RepositoryBase<Student, Guid, StudentEntity, CourseOnlineDbContext>(context), IStudentRepository
 {
+    public async Task<Student?> GetByEmailAsync(string email, CancellationToken ct)
+    {
+        var entity = await Context.Students.AsNoTracking().SingleOrDefaultAsync(s => s.Email == email, ct);
+
+        return entity is null ? default : ToModel(entity);
+    }
+
     protected override StudentEntity ToEntity(Student model)
     {
         return new StudentEntity()
@@ -18,7 +25,7 @@ public class StudentRepository(CourseOnlineDbContext context) : RepositoryBase<S
             LastName = model.LastName,
             Email = model.Email,
             ImageUrl = model.ImageUrl,
-
+            ProgramId = model.ProgramId
         };
     }
     protected override Student ToModel(StudentEntity entity)
