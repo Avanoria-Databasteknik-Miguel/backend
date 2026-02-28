ba962901b92650eb6d7b492ea471291d2e1210e9
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 13:52:09 +0100

    refactor(added get all students):
    - Added mthod for getting all students

--- CourseOnline.Application/Contracts/Students/IStudentService.cs
+++ CourseOnline.Application/Contracts/Students/IStudentService.cs
@@ -7,6 +7,7 @@ public interface IStudentService
 {
     Task<Result<Student>> CreateStudentAsync(CreateStudentInput input, CancellationToken ct);
     Task<Result<Student>> GetStudentByIdAsync(Guid id, CancellationToken ct);
+    Task<Result<IReadOnlyCollection<Student>>> GetStudentsAsync(CancellationToken ct);
     Task<Result<Student>> UpdateStudentAsync(UpdateStudentInput input, CancellationToken ct);
     Task<Result> DeleteStudentAsync(Guid id, CancellationToken ct);
 }
