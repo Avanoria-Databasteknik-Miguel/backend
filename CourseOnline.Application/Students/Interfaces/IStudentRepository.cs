bf050e0dff11137d38eecb1630e42ea8cbec7a41
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 08:34:33 +0100

    feat(IStudentRepository): Implemented with its GetByNameAsync-method.

--- CourseOnline.Application/Students/Interfaces/IStudentRepository.cs
+++ CourseOnline.Application/Students/Interfaces/IStudentRepository.cs
@@ -1,7 +1,9 @@
 ﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Application.Common.Results;
 using CourseOnline.Domain.Models;
 
 namespace CourseOnline.Application.Students.Interfaces;
 public interface IStudentRepository : IRepositoryBase<Student, Guid>
 {
+    Task<Result<Program>> GetByNameAsync(string email, CancellationToken ct);
 }
