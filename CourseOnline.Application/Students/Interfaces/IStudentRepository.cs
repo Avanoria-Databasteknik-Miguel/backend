1138451c0e941900b3866b477caa82818ed4ae3f
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-02-28 08:21:03 +0100

    feat(IStudentRepository):
    - Implemented with IRepositorybase and its domain model

---
+++ CourseOnline.Application/Students/Interfaces/IStudentRepository.cs
@@ -0,0 +1,7 @@
+﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Students.Interfaces;
+public interface IStudentRepository : IRepositoryBase<Student, Guid>
+{
+}
