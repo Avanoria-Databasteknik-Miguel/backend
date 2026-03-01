b3dbf5329523eea1316af1ee6e9ada5f31cb8ad2
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 20:24:36 +0100

    feat(registrations): add registration service contract

---
+++ CourseOnline.Application/Contracts/Registrations/IRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿
+using CourseOnline.Application.Common.Results;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Contracts.Registrations;
+public interface IRegistrationService
+{
+    Task<Result<CourseSessionStudent>> RegisterStudentAsync(Guid courseSessionId, Guid studentId, CancellationToken ct);
+
+    Task<Result> UnregisterStudentAsync(Guid courseSessionId, Guid studentId, CancellationToken ct);
+
+    Task<Result<IReadOnlyCollection<CourseSessionStudent>>> GetRegistrationsBySessionIdAsync(Guid courseSessionId, CancellationToken ct);
+
+    Task<Result<IReadOnlyCollection<CourseSessionStudent>>> GetRegistrationsByStudentIdAsync(Guid studentId, CancellationToken ct);
+
+    Task<Result<int>> CountRegistrationsBySessionIdAsync(Guid courseSessionId, CancellationToken ct);
+}
