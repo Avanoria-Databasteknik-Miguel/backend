87c66fe21f411d014efcfe96e7a1e49b87bf1d8b
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 11:50:20 +0100

    feat(course-sessions): add factory

---
+++ CourseOnline.Application/Factories/CourseSessionFactory.cs
@@ -0,0 +1,8 @@
+﻿using CourseOnline.Application.CourseSessions.DTOs.Inputs;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Factories;
+public static class CourseSessionFactory
+{
+    public static CourseSession Create(CreateCourseSessionInput input) => new(Guid.NewGuid(), input.CourseId, input.ClassroomId, input.StartDateTimeUtc, input.EndDateTimeUtc);
+}
