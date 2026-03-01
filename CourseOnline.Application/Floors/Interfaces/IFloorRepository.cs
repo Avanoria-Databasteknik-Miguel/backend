a765b3e47797116474d802d372c073972745a80f
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 15:30:47 +0100

    feat(GetByLvl): Add method in IFloorRepository to get a floor by level.

---
+++ CourseOnline.Application/Floors/Interfaces/IFloorRepository.cs
@@ -0,0 +1,8 @@
+﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Floors.Interfaces;
+public interface IFloorRepository : IRepositoryBase<Floor, int>
+{
+    Task<Floor?> GetByLevelAsync(string level, CancellationToken ct);
+}
