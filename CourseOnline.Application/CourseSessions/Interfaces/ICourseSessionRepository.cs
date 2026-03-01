39c73a9dd02da96570ebfb3b6e649a53a0e7cb28
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 11:49:59 +0100

    feat(course-sessions): add repository contract

---
+++ CourseOnline.Application/CourseSessions/Interfaces/ICourseSessionRepository.cs
@@ -0,0 +1,9 @@
+﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Application.Common.Results;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.CourseSessions.Interfaces;
+public interface ICourseSessionRepository : IRepositoryBase<CourseSession, Guid>
+{
+    Task<Result<CourseSession>> GetByCourseIdAsync(Guid courseId, CancellationToken ct);
+}
