3fa50825d78e64701dcc3fac7d4800c8f7757ffd
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 11:49:46 +0100

    feat(course-sessions): add create, update and delete input DTO

---
+++ CourseOnline.Application/CourseSessions/DTOs/Inputs/CreateCourseSessionInput.cs
@@ -0,0 +1,3 @@
+﻿namespace CourseOnline.Application.CourseSessions.DTOs.Inputs;
+
+public sealed record CreateCourseSessionInput(Guid CourseId, int ClassroomId, DateTime StartDateTimeUtc, DateTime EndDateTimeUtc);
