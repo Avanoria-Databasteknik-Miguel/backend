85f62f7ee49546d3e7156d19fd059e1dbe83fed1
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-01 15:28:08 +0100

    feat(classrooms): add classroom input DTOs

---
+++ CourseOnline.Application/Contracts/Classrooms/IClassroomService.cs
@@ -0,0 +1,13 @@
+﻿using CourseOnline.Application.Classrooms.DTOs;
+using CourseOnline.Application.Common.Results;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Contracts.Classrooms;
+public interface IClassroomService
+{
+    Task<Result<Classroom>> CreateClassroomAsync(CreateClassroomInput input, CancellationToken ct);
+    Task<Result<Classroom>> UpdateClassroomAsync(UpdateClassroomInput input, CancellationToken ct);
+    Task<Result> DeleteClassroomAsync(DeleteClassroomInput input, CancellationToken ct);
+    Task<Result<Classroom>> GetClassroomByIdAsync(int Id, CancellationToken ct);
+    Task<Result<IReadOnlyCollection<Classroom>>> GetAllClassroomsAsync(CancellationToken ct);
+}
