5b42857566dc4c659601732cd15279e7e990f11f
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 18:13:19 +0100

    feat(reports): implement report service

---
+++ CourseOnline.Application/Services/ReportService.cs
@@ -0,0 +1,14 @@
+﻿using CourseOnline.Application.Reports.DTOs;
+using CourseOnline.Application.Reports.Interfaces;
+
+namespace CourseOnline.Application.Services;
+
+public sealed class ReportService(IReportRepository repo) : IReportService
+{
+    private readonly IReportRepository _repo = repo;
+
+    public Task<IReadOnlyCollection<SessionAvailabilityReport>> GetSessionAvailabilityAsync(CancellationToken ct) => _repo.GetSessionAvailabilityReportAsync(ct);
+
+    public Task<IReadOnlyCollection<StudentUpcomingSessionsReport>> GetStudentWithUpcomingSessionsAsync(CancellationToken ct) => _repo.GetStudentWithUpcomingSessionsAsync(ct);
+    
+}
