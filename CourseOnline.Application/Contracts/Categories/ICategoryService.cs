2c37dce9162606bd9429c42b66936edfd50a1cc9
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 12:54:40 +0100

    feat(add interface): add category service interface

---
+++ CourseOnline.Application/Contracts/Categories/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CourseOnline.Application.Categories.DTOs;
+using CourseOnline.Application.Common.Results;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Contracts.Categories;
+public interface ICategoryService
+{
+    Task<Result<Category>> CreateCategoryAsync(CreateCategoryInput input, CancellationToken ct);
+    Task<Result<Category>> UpdateCategoryAsync(UpdateCategoryInput input, CancellationToken ct);
+    Task<Result> DeleteCategoryAsync(DeleteCategoryInput input, CancellationToken ct);
+    Task<Result<Category>> GetCategoryById(Guid id, CancellationToken ct);
+    Task<IReadOnlyCollection<Category>> GetAllCategories(CancellationToken ct);
+}
