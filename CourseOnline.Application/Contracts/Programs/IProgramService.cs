9bfd2fb49a60f9c18ef530d0a9c57b49bd4e6cfb
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 23:26:23 +0100

    refactor(programs): return ProgramOutput for create/update in IProgramService

--- CourseOnline.Application/Contracts/Programs/IProgramService.cs
+++ CourseOnline.Application/Contracts/Programs/IProgramService.cs
@@ -1,14 +1,15 @@
 ﻿using CourseOnline.Application.Common.Results;
 using CourseOnline.Application.Programs.DTOs.Inputs;
+using CourseOnline.Application.Programs.DTOs.Outputs;
 using CourseOnline.Domain.Models;
 
 namespace CourseOnline.Application.Contracts.Programs;
 public interface IProgramService
 {
-    Task<Result<Program>> CreateProgramAsync(CreateProgramInput input, CancellationToken ct);
+    Task<Result<ProgramOutput>> CreateProgramAsync(CreateProgramInput input, CancellationToken ct);
     Task<Result<Program>> GetProgramByIdAsync(Guid id, CancellationToken ct);
     Task<Result<Program>> GetProgramByNameAsync(string name, CancellationToken ct);
-    Task<Result<Program>> UpdateProgramAsync(UpdateProgramInput input, CancellationToken ct);
+    Task<Result<ProgramOutput>> UpdateProgramAsync(UpdateProgramInput input, CancellationToken ct);
     Task<Result> DeleteProgramAsync(DeleteProgramInput id, CancellationToken ct);
     Task<Result<IReadOnlyCollection<Program>>> GetAllProgramsAsync(CancellationToken ct);
 }
