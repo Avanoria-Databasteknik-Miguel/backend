9a00880286016fd75f6f10738fc571e583bde65a
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 17:25:49 +0100

    feat(student-courses): add student-course service contract

---
+++ CourseOnline.Application/Contracts/StudentCourses/IStudentCourseService.cs
@@ -0,0 +1,12 @@
+﻿using CourseOnline.Application.Common.Results;
+using CourseOnline.Domain.Models;
+
+namespace CourseOnline.Application.Contracts.StudentCourses;
+public interface IStudentCourseService
+{
+    Task<Result> AddStudentToCourseAsync(Guid studentId, Guid courseId, CancellationToken ct);
+    Task<Result> RemoveStudentFromCourseAsync(Guid studentId, Guid courseId, CancellationToken ct);
+    Task<Result<IReadOnlyCollection<Course>>> GetCoursesByStudentIdAsync(Guid studentId, CancellationToken ct);
+    Task<Result<IReadOnlyCollection<Student>>> GetStudentsByCourseIdAsync(Guid courseId, CancellationToken ct);
+
+}
