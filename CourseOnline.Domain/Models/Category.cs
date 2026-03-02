345b8f376d7248ecb26a6da1a9b5f508485684f3
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 12:55:03 +0100

    feat(domain model): add domain model for category

---
+++ CourseOnline.Domain/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using CourseOnline.Domain.Exceptions;
+
+namespace CourseOnline.Domain.Models;
+public sealed class Category
+{
+    public Guid Id { get; }
+    public string Name { get; private set; }
+
+    public Category(Guid id, string name)
+    {
+        if (id == Guid.Empty) throw new DomainValidationException("Id is required");
+        if (string.IsNullOrWhiteSpace(name)) throw new DomainValidationException("Name is required");
+
+        Id = id;
+        Name = name.Trim().ToLower();
+    }
+
+    public void Update(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new DomainValidationException("Name is required");
+
+        Name = name.Trim().ToLower();
+    }
+
+}
