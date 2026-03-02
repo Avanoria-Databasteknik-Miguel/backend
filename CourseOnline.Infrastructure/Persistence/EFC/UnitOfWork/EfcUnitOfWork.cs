f4484dd2e2b858571ffb31f5f2fffde089ca0f4d
DetectiveGarcia <144781010+DetectiveGarcia@users.noreply.github.com>
2026-03-02 23:29:08 +0100

    feat(uow): set CreatedAtUtc/ModifiedAtUtc automatically on save

--- CourseOnline.Infrastructure/Persistence/EFC/UnitOfWork/EfcUnitOfWork.cs
+++ CourseOnline.Infrastructure/Persistence/EFC/UnitOfWork/EfcUnitOfWork.cs
@@ -1,9 +1,36 @@
 ﻿using CourseOnline.Application.Common.Interfaces;
+using CourseOnline.Infrastructure.Common.Entities;
 using CourseOnline.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace CourseOnline.Infrastructure.Persistence.EFC.UnitOfWork;
 
-public class EfcUnitOfWork(CourseOnlineDbContext context) : IUnitOfWork
+public sealed class EfcUnitOfWork(CourseOnlineDbContext context) : IUnitOfWork
 {
-    public Task<int> SaveChangesAsync(CancellationToken ct = default) => context.SaveChangesAsync(ct);
+    public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+    {
+        var now = DateTime.UtcNow;
+
+        foreach (var entry in context.ChangeTracker.Entries<AuditableEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                // Sätt Created bara vid insert
+                if (entry.Entity.CreatedAtUtc == default)
+                    entry.Entity.CreatedAtUtc = now;
+
+                // Oftast vill man även sätt Modified vid insert
+                entry.Entity.ModifiedAtUtc = now;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                // Created ska aldrig ändras vid update
+                entry.Property(x => x.CreatedAtUtc).IsModified = false;
+
+                entry.Entity.ModifiedAtUtc = now;
+            }
+        }
+
+        return await context.SaveChangesAsync(ct);
+    }
 }
